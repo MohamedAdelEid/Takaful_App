70107ad51ae798492bbfbb63a5405a398ebab746
mohamed adel <01200505348mohamed@gmail.com>
2024-07-28 03:04:42 +0300

    - Creare Controller for policy - Create Request for VehicleInsurancePloicy

---
+++ app/Http/Controllers/Campany/PolicyController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers\Campany;
+
+use App\Http\Controllers\Controller;
+use App\Models\Company\Policy;
+use Illuminate\Http\Request;
+
+class PolicyController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function storeVehicleInsurance(Request $request)
+    {
+        
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
