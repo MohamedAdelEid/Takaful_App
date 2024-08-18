69bfcb77e225024b0aa8325f8607757e4400c158
Mohamed Saad <sa3doni2714@gmail.com>
2024-08-18 19:54:11 +0300

    reset

--- app/Notifications/SendResetPasswordNotification.php
+++ app/Notifications/SendResetPasswordNotification.php
@@ -34,7 +34,8 @@ class SendResetPasswordNotification extends Notification
     {
         return (new MailMessage)
             ->line('You are receiving this email because we received a password reset request for your account.')
-            ->action('Reset Password From This Link', $this->code)
+            ->line('Your Reset Password code is:')
+            ->line($this->code)
             ->line('If you did not request a password reset, no further action is required.');
     }
 
